be29d328537fcd52475ea7fb914c2197d0608d46
Joachim Kołodziejski <kolodziejski.joachim@gmail.com>
2023-03-26 20:35:46 +0200

    Add automatic desc table sort by date on page load, add / endpoint redirection to /index.html to dev server (srv.py)

--- srv.py
+++ srv.py
@@ -1,8 +1,12 @@
 # temporary server (only for development)
 
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__, static_folder="docs", static_url_path="/")
+
+@app.route("/")
+def index():
+    return redirect(url_for("static", filename="index.html"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
