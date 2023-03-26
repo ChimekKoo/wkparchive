29ce1939491e769337d7c05c60a05caf5199a8f0
Joachim Kołodziejski <kolodziejski.joachim@gmail.com>
2023-03-26 19:14:57 +0200

    Add school type, fix code snippet in Development section of README, small style change

--- srv.py
+++ srv.py
@@ -2,7 +2,7 @@
 
 from flask import Flask
 
-app = Flask(__name__, static_folder="html", static_url_path="/")
+app = Flask(__name__, static_folder="docs", static_url_path="/")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
